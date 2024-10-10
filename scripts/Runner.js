490906b90eb7041d18d6feee888d1ea8b3c64caf
Jazmin Bascuñan <jazbascunan01@gmail.com>
2024-10-10 17:55:16 -0300

    funcionando con el homero dormido

--- scripts/Runner.js
+++ scripts/Runner.js
@@ -23,48 +23,48 @@ class Runner extends Personaje {
     saltar() {
         if (!this.personaje.classList.contains("saltar") && !this.personaje.classList.contains("caer")) {
             const estabaCorriendo = this.corriendo;  // Guarda si estaba corriendo antes del salto
-    
+
             this.clean();
             this.personaje.classList.add("saltar");
-    
+
             this.personaje.addEventListener("animationend", () => {
                 this.caer(estabaCorriendo);
             });
         }
     }
-    
+
 
     quieto() {
-        if (this.corriendo) {
-            this.clean();
-            this.personaje.classList.add("quieto");
-            this.corriendo = false;
-            this.moverFondo(false);  // Detener el fondo
-        }
+        this.clean();
+        this.personaje.classList.add("quieto");
+        this.corriendo = false;
+        this.moverFondo(false);  // Detener el fondo
     }
     caer(estabaCorriendo) {
         this.clean();
         this.personaje.classList.add("caer");
-    
+
         this.personaje.addEventListener("animationend", () => {
             if (estabaCorriendo) {
+                console.log(estabaCorriendo);
                 this.correr();
             } else {
                 this.quieto();
+                console.log(estabaCorriendo);
             }
         });
     }
-    
+
 
     /**
      * 
      */
     clean() {
-        this.personaje.classList.remove("correr"); 
+        this.personaje.classList.remove("correr");
         this.personaje.classList.remove("saltar");
         this.personaje.classList.remove("caer");
         this.personaje.classList.remove("quieto");
-        this.personaje.removeEventListener("animationend", () => {}); 
+        this.personaje.removeEventListener("animationend", () => { });
     }
     moverFondo(activar) {
         const elementosFondo = ['sky', 'clouds', 'fondoLejano', 'vereda', 'montania'];
@@ -73,5 +73,5 @@ class Runner extends Personaje {
             elem.style.animationPlayState = activar ? 'running' : 'paused';
         });
     }
-    
+
 }
